31113cb6d24a2769429f7372de1afe4f6e208671
Rahul Aggarwal <rahulaggarwal@Rahuls-MacBook-Air.local>
2023-12-13 02:27:22 +0530

    Feat: Adding-authentication

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { FC, useState } from "react";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { ToastAction } from "@/components/ui/Toast";
+import { useToast } from "@/hooks/use-toast";
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...prop }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (err) {
+      toast({
+        title: "There was a problem.",
+        description: "There was error logging in with google",
+        variant: "destructive",
+        action: <ToastAction altText="Try again">Try again</ToastAction>,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className={cn("flex justify-center", className)} {...prop}>
+      <Button
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="w-4 h-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
